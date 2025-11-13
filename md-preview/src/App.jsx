031197a9c53939eff17167d19ba6d9c8c5305f71
ProishTheIdiot <64977705+thepoopooman652@users.noreply.github.com>
2025-11-13 08:34:42 -0500

    Add files via upload

---
+++ md-preview/src/App.jsx
@@ -0,0 +1,15 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <Content />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
